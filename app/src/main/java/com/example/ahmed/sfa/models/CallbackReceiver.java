1e7c0afe7758644eac24372cf87170435d814ece
asankagit <inbox2asanka@gmail.com>
2017-03-25 13:13:33 +0530

    initialLoginUI

---
+++ app/src/main/java/com/example/ahmed/sfa/models/CallbackReceiver.java
@@ -0,0 +1,9 @@
+package com.example.ahmed.sfa.models;
+
+/**
+ * Created by DELL on 3/25/2017.
+ */
+public interface CallbackReceiver {
+    public void receiveData(Object result);
+
+}
--- app/src/main/java/com/example/ahmed/sfa/models/JsonHelper.java
+++ app/src/main/java/com/example/ahmed/sfa/models/JsonHelper.java
@@ -1,7 +1,8 @@
 package com.example.ahmed.sfa.models;
 
-import android.database.Cursor;
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,10 +14,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * Created by DELL on 3/18/2017.
@@ -26,33 +29,79 @@ public class JsonHelper {
     ArrayList<String> jsonMyArray=new ArrayList<String>();
 
     TextView result_view;
+    private  boolean isLonding=true;
+    private  boolean isRequesting=false;
+    private  String filterType="deviceid_pass";
+    private  String  activeStatus="no";
 
     public JsonHelper(TextView tv){
         result_view=tv;
     }
+
+    public  String initialLoging(String devideId,String pass){
+
+        final String[] recieveData = new String[1];
+        new JsonDataCallback() {
+            @Override
+            public void receiveData(Object object) {
+                String tmpData = (String)object;
+                result_view.setText(tmpData);
+
+                try {
+                    JSONArray jsonArray = new JSONArray(tmpData);
+                    JSONObject jsonObject=jsonArray.getJSONObject(0);
+                    recieveData[0] =jsonObject.optString("ACTIVESTATUS");
+                    result_view.setText(jsonObject.optString("ACTIVESTATUS"));
+                    setLonding(false);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.execute("http://www.bizmapexpert.com/api/DeviceCheck/DeviceCheckController?DeviceID="+devideId+"&Password="+pass+"",null,null);
+        filterType="deviceid_pass";
+
+
+
+
+
+
+        return recieveData[0];
+    }
+
+
+
 
 
     public  void sendInitialData(String devideId,String pass){
 
 
         new HttpAsyncTask().execute("http://www.bizmapexpert.com/api/ProductBrandManagement/SelectProductBrandManagement?DeviceID=T1&RepID=93");
-
+        filterType="salesProductBrand";
     }
 
+    public boolean getisLonding() {
+        return isLonding;
+    }
+
+    public void setLonding(boolean londing) {
+        isLonding = londing;
+    }
 
 
     private class HttpAsyncTask extends AsyncTask<String,Void,String> {
         @Override
         protected String doInBackground(String... urls){
-
+            setLonding(true);
             return GET(urls[0]);
         }
 
         //onPostExecute displays the result of the asyncTask
         @Override
         protected void onPostExecute(String result){
-
+            setLonding(false);
             result_view.setText(jsonMyArray.get(0));
+
 
         }
     }
@@ -81,26 +130,9 @@ public class JsonHelper {
                 result = convertInputStreamToString(in);
                 /**/
                 JSONObject jsonRootObject = null;
+                filterJsonData(result) ;
 
-                try {
-                    //jsonRootObject = new JSONObject(result);
 
-                    //Get the instance of JSONArray that contains JSONObjects
-                    //JSONArray jsonArray = jsonRootObject.optJSONArray("snappedPoints");
-                    JSONArray jsonArray = new JSONArray(result);
-
-                    //Iterate the jsonArray and print the info of JSONObjects
-                    for(int i=0; i < jsonArray.length(); i++){
-                        JSONObject jsonObject = jsonArray.getJSONObject(i);
-
-                        String  principle = jsonObject.optString("Principle").toString();
-                        String mainBrand = jsonObject.optString("MainBrand").toString();
-                        jsonMyArray.add(mainBrand);
-                    }
-
-                } catch (JSONException e) {
-                    // e.printStackTrace();
-                }
                 /**/
 
             }else{
@@ -115,6 +147,36 @@ public class JsonHelper {
 
         return result;
     }
+
+    private void filterJsonData(String result) {
+        try {
+
+            if(filterType=="salesProductBrand") {
+                JSONArray jsonArray = new JSONArray(result);
+
+                //Iterate the jsonArray and print the info of JSONObjects
+
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    String principle = jsonObject.optString("Principle").toString();
+                    String mainBrand = jsonObject.optString("MainBrand").toString();
+                    jsonMyArray.add(mainBrand);
+                }
+            }else if(filterType=="deviceid_pass"){
+                JSONArray jsonArray = new JSONArray(result);
+                JSONObject jsonObject = jsonArray.getJSONObject(0);
+                String status=jsonObject.optString("ACTIVESTATUS");
+                activeStatus=status;
+            }
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
     //
     private static String convertInputStreamToString(InputStream inputStream) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
@@ -257,6 +319,52 @@ public class JsonHelper {
 
         public void setLastUpdateDae(String lastUpdateDae) {
             this.lastUpdateDae = lastUpdateDae;
+        }
+    }
+
+    //abstract class to check dara return
+    public abstract class JsonDataCallback extends AsyncTask<String, String, String> implements CallbackReceiver {
+        /*private ProgressDialog mProgressDialog;
+        Handler handler;
+        Runnable callback;
+        Activity activity;*/
+
+
+       /* public JsonDataCallback(Activity activity)
+        {
+            this.activity=activity;
+            mProgressDialog = new ProgressDialog(activity);
+            mProgressDialog.setMessage("Loading Please Wait.");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.setMax(100);
+            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            mProgressDialog.setCancelable(true);
+        }*/
+
+        public abstract void receiveData(Object object);
+        @Override
+        protected void onPreExecute() {
+            //mProgressDialog =ProgressDialog.show(activity, "", "Please Wait",true,false);
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            return GET(urls[0]);
+            // do stuff
+
+        }
+
+
+        @Override
+        protected void onPostExecute(String jsonData) {
+            /*if (mProgressDialog != null || mProgressDialog.isShowing()){
+                mProgressDialog.dismiss();
+            }*/
+            if(jsonData!=null)
+            {
+                receiveData(jsonData);
+            }
         }
     }
 }
